982789a1cd298f60fcceacce8540ef73ff036bcd
Arthur Colman <arthurcolman@poli.ufrj.br>
2022-02-22 01:10:10 -0300

    add city input

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -1,33 +1,62 @@
-import React, { useEffect, useState, useMemo } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import Form from 'react-bootstrap/Form';
 
 const CityInput = (props) => {
-  const [ currentPosition, setCurrentPosition ] = useState();
-  const [ weatherInfo, setWeatherInfo ] = useState();
+  const {
+    setWeatherInfo
+  } = props;
+
+  const [ currentCity, setCurrentCity ] = useState('');
+  const [ loading, setLoading ] = useState(false);
 
   useEffect(() => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(async ({ coords: { latitude, longitude }}) => {
-        setCurrentPosition(await axios({
+        setCurrentCity((await axios({
           method: 'get',
           url: `http://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=5&appid=${process.env.API_KEY}`
-        }));
-        setWeatherInfo(await axios({
-          method: 'get',
-          url: `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${process.env.API_KEY}`
-        }));
+        })).data[0].name);
       })
     }
-  }, [])
+  }, [setWeatherInfo])
 
-  console.log('WEATHER INFO', weatherInfo);
-  console.log('CURRENT POSTION', currentPosition);
+  useEffect(() => {
+    const getCityPosition = async () => {
+      const cityPosition = await axios({
+        method: 'get',
+        url: `http://api.openweathermap.org/geo/1.0/direct?q=${currentCity}&limit=5&appid=${process.env.API_KEY}`,
+      });
 
-  const currentCountry = useMemo(() => currentPosition ? currentPosition.data[0].name : '', [currentPosition])
+      if (cityPosition && cityPosition.data) {
+        const {
+          lat,
+          lon,
+        } = cityPosition.data[0]
+        setWeatherInfo(await axios({
+          method: 'get',
+          url: `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.API_KEY}`
+        }));
+      }
+    }
+
+    if (!loading && currentCity) {
+      getCityPosition()
+    }
+  }, [loading, currentCity])
   
   return (
     <div>
-      {currentCountry}
+      <span>Sua Cidade</span>
+      <Form.Control
+        type='text'
+        defaultValue={currentCity}
+        onChange={(v) => {
+          setLoading(true);
+          setCurrentCity(v.target.value);
+          setTimeout(() => setLoading(false), 3000);
+        }}
+      />
     </div>
   )
 };
--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import CityInput from './input.jsx';
 
 const Modal = (props) => {
+  const [ weatherInfo, setWeatherInfo ] = useState();
+
+  console.log('WEATHER', weatherInfo)
   return (
     <div className='modal'>
-      <CityInput />
+      <CityInput setWeatherInfo={setWeatherInfo} />
     </div>
   )
 }
