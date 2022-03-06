29f187e6ef5007cac1924ebd9a4be531bc45504b
Arthur Colman <arthurcolman@poli.ufrj.br>
2022-03-05 21:21:44 -0300

    wip

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
     main: path.resolve(__dirname, './src/index.js'),
   },
   output: {
-    path: path.resolve(__dirname, './docs'),
+    path: path.resolve(__dirname, './build'),
     filename: '[name].bundle.js',
     publicPath: '/',
   },
